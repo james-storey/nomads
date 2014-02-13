d541f6f28e6a40c3674c5c6831c3d4ee793893ec
James Storey <storey.james@gmail.com>
2014-02-13 04:01:03 -0600

    refactored to multiple files

---
+++ Controls.js
@@ -0,0 +1,213 @@
+var handleMouse = function() {
+	var hold = {};
+	var selectBoxElem = document.getElementById("select-box");
+	var selectableObjects = Nomads.selectableObjects;
+	var selected = Nomads.selected;
+	var projector = new THREE.Projector();
+
+	var that = {};
+
+	var down = function(event) {
+		hold['down'] = new THREE.Vector2(event.clientX, event.clientY);
+		// anchor selection aid
+		selectBoxElem.style.top = event.clientY;
+		selectBoxElem.style.left = event.clientX;
+	};
+
+	var up = function(event) {
+		var down = hold['down'];
+		var camera = Nomads.camera;
+		var clickZone = 0.5;
+		for (var sO = selected.length - 1; sO >= 0; sO -= 1) {
+			(selected.pop()).deselect();
+		}
+		if (Math.abs(down.x - event.clientX) < clickZone && 
+			Math.abs(down.y - event.clientY) < clickZone) {
+			// raycast select
+			var vector = new THREE.Vector3((down.x / window.innerWidth) * 2 - 1, 
+				-(down.y / window.innerHeight) * 2 + 1, 0.5);
+			projector.unprojectVector(vector, camera);
+
+			var raycaster = new THREE.Raycaster(camera.position,
+				vector.sub(camera.position).normalize());
+			var intersect = [];
+			var sO = 0, intersectIndex;
+			for (sO = selectableObjects.length - 1; sO >= 0 && intersect.length < 1; sO -= 1) {
+				intersect = raycaster.intersectObject(selectableObjects[sO].Mesh);
+				intersectIndex = sO;
+			}
+			if (intersect.length > 0) {
+				selected.push(selectableObjects[intersectIndex]);
+				selectableObjects[intersectIndex].select();
+			}
+		}
+		else {
+			// bounded select
+			for (var oi = 0, ol = selectableObjects.length; oi < ol; oi ++){
+				// project center of object to screen
+				var obj = selectableObjects[oi].Mesh;
+				var posCopy = new THREE.Vector3();
+				posCopy.copy(obj.position);
+
+				// normalized centered coords
+				var first = new THREE.Vector2((down.x / window.innerWidth) * 2 - 1,
+					-(down.y / window.innerHeight) * 2 + 1);
+				var second = new THREE.Vector2((event.clientX / window.innerWidth) * 2 - 1,
+					-(event.clientY/window.innerHeight) * 2 + 1);
+				var min = new THREE.Vector2(Math.min(first.x, second.x), Math.min(first.y, second.y));
+				var max = new THREE.Vector2(Math.max(first.x, second.x), Math.max(first.y, second.y));
+				var selectBox = new THREE.Box2(min, max);
+				
+				var screenCoord = projector.projectVector(posCopy, camera);
+				//get size of projected bounding sphere
+				var gC = new THREE.Vector3();
+				gC.copy(obj.position);
+				gC.sub(camera.position);
+				var gCircle = (new THREE.Vector3()).crossVectors(gC, new THREE.Vector3(0,1,0)).normalize();
+				gCircle.multiplyScalar(obj.geometry.boundingSphere.radius * 2);
+				gCircle.add(obj.position);
+				var gCircleScreen = projector.projectVector(gCircle, camera);
+				var objBoxSize = (new THREE.Vector2(gCircleScreen.x, gCircleScreen.y)).sub(screenCoord).length();
+
+				var objBox = new THREE.Box2();
+				objBox.setFromCenterAndSize(new THREE.Vector2(screenCoord.x, screenCoord.y), 
+					new THREE.Vector2(objBoxSize, objBoxSize));
+				
+				// fudge with bounding sphere radius
+				if (selectBox.isIntersectionBox(objBox) || selectBox.containsBox(objBox)){
+					// inside bounds, add to selection
+					selected.push(selectableObjects[oi]);
+					selectableObjects[oi].select();
+
+				}
+			}	
+		}
+		selectBoxElem.style.visibility = "hidden";
+		hold['down'] = null;
+	};
+
+	var move = function(event) {
+		hold['mouseX'] = event.clientX;
+		hold['mouseY'] = event.clientY;
+		if (hold['down'] instanceof THREE.Vector2) {
+			selectBoxElem.style.visibility = "visible";
+		}
+	};
+
+	var update = function() {
+		if (hold['down'] instanceof THREE.Vector2) {
+			var mouseX = hold['mouseX'];
+			var mouseY = hold['mouseY'];
+			var d = hold['down'];
+
+			if(mouseY < d.y) {
+				selectBoxElem.style.top = mouseY;
+				selectBoxElem.style.height = d.y - mouseY;
+			}
+			else { 
+				selectBoxElem.style.height = mouseY - d.y;
+			}
+
+			if(mouseX < d.x) {
+				selectBoxElem.style.left = mouseX;
+				selectBoxElem.style.width = d.x - mouseX;
+			}
+			else {
+				selectBoxElem.style.width = mouseX - d.x;
+			}
+		}
+	};
+
+	that.down = down;
+	that.up = up;
+	that.move = move;
+	that.update = update;
+
+	return that;
+}();
+
+var handleKey = function() {
+	var hold = {};
+	var camera;
+	var cameraCenter = new THREE.Vector3();
+	var cameraSpeed = 0.5;
+
+	var that = {};
+
+	var down = function(event){
+		hold[event.keyCode] = true;
+	};
+
+	var up = function(event){
+		hold[event.keyCode] = false;
+	};
+
+	var update = function() {
+		camera = Nomads.camera;
+		var translateSpeed = cameraSpeed * 1.4;
+		var axis = new THREE.Vector3();
+		axis.copy(cameraCenter).sub(camera.position);
+		axis.y = 0;
+		axis.normalize();
+		rightAxis = new THREE.Vector3().copy(axis).cross(new THREE.Vector3(0,1,0)).normalize();
+		if (hold["W".charCodeAt(0)] === true)
+		{
+			var forward = new THREE.Vector3().copy(axis);
+			forward.multiplyScalar(translateSpeed);
+			camera.position.add(forward);
+			cameraCenter.add(forward);
+		}
+		if (hold["S".charCodeAt(0)] === true)
+		{
+			var back = new THREE.Vector3().copy(axis);
+			back.multiplyScalar(-translateSpeed);
+			camera.position.add(back);
+			cameraCenter.add(back);
+		}
+		if (hold["A".charCodeAt(0)] === true)
+		{
+			var left = new THREE.Vector3().copy(rightAxis);
+			left.multiplyScalar(-translateSpeed);
+			camera.position.add(left);
+			cameraCenter.add(left);
+		}
+		if (hold["D".charCodeAt(0)] === true)
+		{
+			var right = new THREE.Vector3().copy(rightAxis);
+			right.multiplyScalar(translateSpeed);
+			camera.position.add(right);
+			cameraCenter.add(right);
+		}
+
+		var rotateCamera = function(CCW) {
+			var dir = 1, d;
+			if(CCW !== true){
+				dir = -1;
+			}
+			d = new THREE.Vector2(camera.position.x - cameraCenter.x, camera.position.z - cameraCenter.z);
+			rotSpeed = cameraSpeed * 0.05;
+			camera.position.x = cameraCenter.x + d.x * Math.cos(rotSpeed) - 
+				d.y*Math.sin(rotSpeed) * dir;
+			camera.position.z = cameraCenter.z + d.x * Math.sin(rotSpeed) * dir + 
+				d.y*Math.cos(rotSpeed);
+		};
+		if(hold["Q".charCodeAt(0)] === true)
+		{
+			// orbit CCW
+			rotateCamera(true);
+			camera.lookAt(cameraCenter);
+		}
+		if(hold["E".charCodeAt(0)] === true)
+		{
+			// orbit CW
+			rotateCamera(false);
+			camera.lookAt(cameraCenter);
+		}
+	};
+
+	that.up = up;
+	that.down = down;
+	that.update = update;
+
+	return that;
+}();
---
+++ Program.js
@@ -0,0 +1,115 @@
+
+var Program = function() {
+	var world, body, mass, shape, timeStep = 1/60;
+
+	var that = {};
+
+	var initThree = function() {
+		Nomads.scene = new THREE.Scene();
+		Nomads.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+		Nomads.camera.translateZ(100);
+		Nomads.camera.translateY(100);
+		Nomads.camera.lookAt(new THREE.Vector3());
+		
+		Nomads.scene.add(Nomads.camera);
+
+		Nomads.renderer = Detector.webgl? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
+		Nomads.renderer.setClearColor(0x8096da, 1);
+		Nomads.renderer.setSize(window.innerWidth, window.innerHeight);
+
+		var viewport = Nomads.renderer.domElement;
+		document.body.appendChild(viewport);
+		window.addEventListener('resize', onResize, false);
+		window.addEventListener('mousedown', handleMouse.down, false);
+		window.addEventListener('mouseup', handleMouse.up, false);
+		window.addEventListener('keydown', handleKey.down, false);
+		window.addEventListener('keyup', handleKey.up, false);
+		window.addEventListener('mousemove', handleMouse.move, false);
+
+		// init geo
+
+		var planeGeo = new THREE.PlaneGeometry(100, 100, 10, 10);
+		//for (var i = planeGeo.vertices.length - 1; i >= 0; i--) {
+		//	var v = planeGeo.vertices[i];
+		//	v.z = Math.sin(v.x/10)*5 - Math.cos(v.y/10)*5;
+		//};
+		planeGeo.computeFaceNormals();
+		planeGeo.computeVertexNormals();
+
+		var mat = new THREE.MeshLambertMaterial({color: 0xffffff});
+		Nomads.terrainMesh = new THREE.Mesh(planeGeo, mat);
+		Nomads.terrainMesh.position = new THREE.Vector3(0, 0, 0);
+		Nomads.terrainMesh.rotateX(-Math.PI/2);
+		//terrainMesh.rotateY(1);
+		Nomads.scene.add(Nomads.terrainMesh);
+
+		var aL = new THREE.AmbientLight( 0x404040 );
+		var dL = new THREE.DirectionalLight({color: 0xffffff});
+		dL.position = new THREE.Vector3( 1, 1, 1 );
+		Nomads.scene.add(aL);
+		Nomads.scene.add(dL);
+
+		var objMat = new THREE.MeshLambertMaterial( {color: 0xda9680} );
+		var objGeo = new THREE.CubeGeometry( 10, 10, 10, 1, 1, 1);
+		var objMesh = new THREE.Mesh(objGeo, objMat);
+		objMesh.position.y = 5;
+		Nomads.scene.add(objMesh);
+
+		Nomads.selectableObjects.push(new SelectableObject(objMesh));
+
+		/*var helper = new THREE.AxisHelper();
+		helper.scale = new THREE.Vector3(10, 10, 10);
+		helper.position.y = 1;
+		terrainMesh.add(helper);*/
+
+	};
+
+	var initCannon = function() {
+		world = new CANNON.World();
+		world.gravity.set(0,0,0);
+		world.broadphase = new CANNON.NaiveBroadphase();
+		world.solver.iterations = 10;
+
+	};
+
+	var init = function() {
+		
+		initThree();
+		initCannon();
+	};
+
+	var update = function() {
+		requestAnimationFrame( update );
+        updatePhysics();
+        handleKey.update();
+        handleMouse.update();
+        Nomads.selectableObjects.forEach(function(obj){obj.update();});
+        render();
+	};
+
+	var render = function() {
+		Nomads.renderer.render(Nomads.scene, Nomads.camera);
+	};
+
+	var updatePhysics = function() {
+		world.step(timeStep);
+		// copy coords from CANNON objects to THREE child objects
+
+	};
+
+	var onResize = function() {
+		Nomads.camera.aspect = window.innerWidth / window.innerHeight;
+		Nomads.camera.updateProjectionMatrix();
+
+		Nomads.renderer.setSize(window.innerWidth, window.innerHeight);
+	};
+
+	that.init = init;
+	that.update = update;
+
+	return that;
+};
+
+var program = new Program();
+program.init();
+program.update();
