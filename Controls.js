1f910919e0ac495573a96f405fc0cc6615107daa
James Storey <storey.james@gmail.com>
2014-03-15 12:58:59 -0500

    removed box select for game design adjustments

--- Controls.js
+++ Controls.js
@@ -1,6 +1,5 @@
 var handleMouse = function() {
 	var hold = {};
-	var selectBoxElem = document.getElementById("select-box");
 	var selectableObjects = Nomads.selectableObjects;
 	var selected = Nomads.selected;
 	var projector = new THREE.Projector();
@@ -20,22 +19,6 @@ var handleMouse = function() {
 
 	var down = function(event) {
 		hold['down'] = new THREE.Vector2(event.clientX, event.clientY);
-		// anchor selection aid
-		if(event.which === 1) { //LEFT_BUTTON
-			selectBoxElem.style.top = event.clientY;
-			selectBoxElem.style.left = event.clientX;
-		}
-		else if(event.which === 3) { //RIGHT_BUTTON
-			//move selected to location
-			var raycaster = rayCastScreenLocation(event.clientX, event.clientY);
-			var intersectPoint = raycaster.intersectObject(Nomads.terrainMesh)[0].point;
-
-			// create move helper
-			Nomads.UI.moveHelper(intersectPoint);
-			
-			// signal selected to deal with order
-		}
-		
 	};
 
 	var up = function(event) {
@@ -45,96 +28,28 @@ var handleMouse = function() {
 		for (var sO = selected.length - 1; sO >= 0; sO -= 1) {
 			(selected.pop()).deselect();
 		}
-		if (Math.abs(down.x - event.clientX) < clickZone && 
-			Math.abs(down.y - event.clientY) < clickZone) {
-			// raycast select
-			var raycaster = rayCastScreenLocation(down.x, down.y);
-			var intersect = [];
-			var sO = 0, intersectIndex;
-			for (sO = selectableObjects.length - 1; sO >= 0 && intersect.length < 1; sO -= 1) {
-				intersect = raycaster.intersectObject(selectableObjects[sO].Mesh);
-				intersectIndex = sO;
-			}
-			if (intersect.length > 0) {
-				selected.push(selectableObjects[intersectIndex]);
-				selectableObjects[intersectIndex].select();
-			}
+		// raycast select
+		var raycaster = rayCastScreenLocation(down.x, down.y);
+		var intersect = [];
+		var sO = 0, intersectIndex;
+		for (sO = selectableObjects.length - 1; sO >= 0 && intersect.length < 1; sO -= 1) {
+			intersect = raycaster.intersectObject(selectableObjects[sO].Mesh);
+			intersectIndex = sO;
 		}
-		else {
-			// bounded select
-			for (var oi = 0, ol = selectableObjects.length; oi < ol; oi ++){
-				// project center of object to screen
-				var obj = selectableObjects[oi].Mesh;
-				var posCopy = new THREE.Vector3();
-				posCopy.copy(obj.position);
-
-				// normalized centered coords
-				var first = new THREE.Vector2((down.x / window.innerWidth) * 2 - 1,
-					-(down.y / window.innerHeight) * 2 + 1);
-				var second = new THREE.Vector2((event.clientX / window.innerWidth) * 2 - 1,
-					-(event.clientY/window.innerHeight) * 2 + 1);
-				var min = new THREE.Vector2(Math.min(first.x, second.x), Math.min(first.y, second.y));
-				var max = new THREE.Vector2(Math.max(first.x, second.x), Math.max(first.y, second.y));
-				var selectBox = new THREE.Box2(min, max);
-				
-				var screenCoord = projector.projectVector(posCopy, camera);
-				//get size of projected bounding sphere
-				var gC = new THREE.Vector3();
-				gC.copy(obj.position);
-				gC.sub(camera.position);
-				var gCircle = (new THREE.Vector3()).crossVectors(gC, new THREE.Vector3(0,1,0)).normalize();
-				gCircle.multiplyScalar(obj.geometry.boundingSphere.radius * 2);
-				gCircle.add(obj.position);
-				var gCircleScreen = projector.projectVector(gCircle, camera);
-				var objBoxSize = (new THREE.Vector2(gCircleScreen.x, gCircleScreen.y)).sub(screenCoord).length();
-
-				var objBox = new THREE.Box2();
-				objBox.setFromCenterAndSize(new THREE.Vector2(screenCoord.x, screenCoord.y), 
-					new THREE.Vector2(objBoxSize, objBoxSize));
-				
-				// fudge with bounding sphere radius
-				if (selectBox.isIntersectionBox(objBox) || selectBox.containsBox(objBox)){
-					// inside bounds, add to selection
-					selected.push(selectableObjects[oi]);
-					selectableObjects[oi].select();
-
-				}
-			}	
+		if (intersect.length > 0) {
+			selected.push(selectableObjects[intersectIndex]);
+			selectableObjects[intersectIndex].select();
 		}
-		selectBoxElem.style.visibility = "hidden";
 		hold['down'] = null;
 	};
 
 	var move = function(event) {
 		hold['mouseX'] = event.clientX;
 		hold['mouseY'] = event.clientY;
-		if (hold['down'] instanceof THREE.Vector2) {
-			selectBoxElem.style.visibility = "visible";
-		}
 	};
 
 	var update = function() {
-		if (hold['down'] instanceof THREE.Vector2) {
-			var mouseX = hold['mouseX'];
-			var mouseY = hold['mouseY'];
-			var d = hold['down'];
 
-			if(mouseY < d.y) {
-				selectBoxElem.style.top = mouseY;
-				selectBoxElem.style.height = d.y - mouseY;
-			}
-			else { 
-				selectBoxElem.style.height = mouseY - d.y;
-			}
-
-			if(mouseX < d.x) {
-				selectBoxElem.style.left = mouseX;
-				selectBoxElem.style.width = d.x - mouseX;
-			}
-			else {
-				selectBoxElem.style.width = mouseX - d.x;
-			}
-		}
 	};
 
 	that.down = down;
